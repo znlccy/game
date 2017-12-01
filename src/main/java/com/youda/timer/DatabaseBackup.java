3ff62d3857dea2c9ee01a4e50e0b90cdcca2ff36
znlccy <756311868@qq.com>
2017-12-01 18:05:53 +0800

    增加数据库定时备份功能

---
+++ src/main/java/com/youda/timer/DatabaseBackup.java
@@ -0,0 +1,37 @@
+package com.youda.timer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author chencongye
+ * @version 1.0.0
+ * @date 2017-12-01
+ * @introduce 定义数据库备份实体类
+ */
+
+public class DatabaseBackup {
+	
+	@Autowired
+	private ReadConfiguration readConfiguration;
+	
+	/**
+	 * 定义并实现数据库备份
+	 * @throws Exception
+	 */
+	public void BackupDatabase() throws Exception {
+		// TODO Auto-generated method stub
+		/*readConfiguration.getDatasource();*/
+		System.err.println(readConfiguration);
+		/*System.err.println(readConfiguration.getDatasource());*/
+	}
+	
+	public static void main(String[] args) {
+		DatabaseBackup backup = new DatabaseBackup();
+		try {
+			backup.BackupDatabase();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/main/java/com/youda/timer/ReadConfiguration.java
@@ -0,0 +1,27 @@
+package com.youda.timer;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author chencongye
+ * @version 1.0.0
+ * @date 2017-12-01
+ * @introduce 读取配置文件
+ */
+
+
+public class ReadConfiguration {
+	
+	private Map<String, String> datasource = new HashMap();
+
+	public Map<String, String> getDatasource() {
+		return datasource;
+	}
+	
+	public void setDatasource(Map<String, String> datasource) {
+		this.datasource = datasource;
+	}
+}
