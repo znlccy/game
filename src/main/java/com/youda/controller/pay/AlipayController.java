2951e47b12967e0485983d070222b09de06b4eeb
znlccy <756311868@qq.com>
2017-12-11 16:50:21 +0800

    增加微信支付的控制器

--- src/main/java/com/youda/controller/pay/AlipayController.java
+++ src/main/java/com/youda/controller/pay/AlipayController.java
@@ -1,26 +1,20 @@
-package com.youda.controller;
+package com.youda.controller.pay;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.youda.interceptor.UserInterceptor;
-
 /**
  * @author chencongye
  * @version 1.0.0
  * @date 2017-11-28
- * @introduce 定义微信支付的控制器
+ * @introduce 定义支付宝支付的控制器
  */
 
 @RestController
-@RequestMapping(value = "wechat")
+@RequestMapping(value = "/alipay")
 @CrossOrigin(maxAge = 3600, origins = "*")
-public class WeChatPayController   {
-	
-	@RequestMapping(value = "apppay", method = RequestMethod.POST)
-	public String WeChatAppPay() {
-		return "nihao";
-	}
+public class AlipayController   {
+
 }
