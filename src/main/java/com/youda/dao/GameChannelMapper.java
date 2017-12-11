8293ad51f44f8a642e44200049344d2e81410413
znlccy <756311868@qq.com>
2017-12-11 09:51:16 +0800

    增加游戏渠道dao层和用户游戏dao层

---
+++ src/main/java/com/youda/dao/GameChannelMapper.java
@@ -0,0 +1,15 @@
+package com.youda.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Author Chencongye
+ * @Date 2017/12/11 9:45
+ * @Version 1.0.0
+ * @Instructions
+ */
+
+@Mapper
+public interface GameChannelMapper {
+
+}
---
+++ src/main/java/com/youda/dao/UserGameMapper.java
@@ -0,0 +1,15 @@
+package com.youda.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Author Chencongye
+ * @Date 2017/12/11 9:42
+ * @Version 1.0.0
+ * @Instructions 定义用户游戏Dao层接口
+ */
+
+@Mapper
+public interface UserGameMapper {
+
+}
