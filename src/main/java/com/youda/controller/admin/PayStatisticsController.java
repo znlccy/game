86379de33dc505bfc756866e64fe8664b32881ab
znlccy <756311868@qq.com>
2017-12-13 16:12:09 +0800

    添加用户统计和付费统计

---
+++ src/main/java/com/youda/controller/admin/PayStatisticsController.java
@@ -0,0 +1,11 @@
+package com.youda.controller.admin;
+/**
+ * @Author Chencongye
+ * @Date 2017/12/13 16:10
+ * @Version 1.0.0
+ * @Instructions 实现付费和收益统计
+ */
+
+public class PayStatisticsController {
+    
+}
