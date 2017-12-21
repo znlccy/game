e24e0bed74fcde01090476168539fdca740262a6
znlccy <756311868@qq.com>
2017-12-21 18:15:13 +0800

    实现支付宝H5支付的基本框架

--- src/main/java/com/youda/dao/admin/EquipmentStatisticsMapper.java
+++ src/main/java/com/youda/dao/admin/EquipmentStatisticsMapper.java
@@ -18,11 +18,41 @@ import java.util.List;
 public interface EquipmentStatisticsMapper {
 
     /*实现今天新增设备的统计*/
-    @Select("select * from tb_user")
+    @Select("SELECT DATE(dday) ddate,COUNT(*) -2 AS newEquipmentCount     \n" +
+            "FROM       \n" +
+            "(       \n" +
+            "    SELECT datelist AS dday    \n" +
+            "    FROM       \n" +
+            "    tb_calendar        \n" +
+            "    -- 这里是限制返回最近一天的数据             \n" +
+            "    WHERE CONCAT(CURDATE(),' 00:00:00')<= DATE(datelist)&&DATE(datelist)<=CONCAT(CURDATE(),' 24:00:00')      \n" +
+            "    UNION ALL       \n" +
+            "    SELECT userLoginTime    \n" +
+            "    FROM        \n" +
+            "    tb_user       \n" +
+            "    WHERE userLoginTime >=CONCAT(CURDATE(),' 00:00:00') AND userLoginTime<=CONCAT(CURDATE(),' 24:00:00') AND userUseDevice <> NULL OR userUseDevice <> ''      \n" +
+            "    GROUP BY userLoginTime       \n" +
+            "    ) a       \n" +
+            "GROUP BY ddate")
     List<EquipmentStatisticsResponse> todayEquipmentStatistics();
 
     /*实现昨天新增设备的统计*/
-    @Select("select * from tb_user")
+    @Select("SELECT DATE(dday) ddate,COUNT(*) -2 AS newEquipmentCount     \n" +
+            "FROM       \n" +
+            "(       \n" +
+            "    SELECT datelist AS dday    \n" +
+            "    FROM       \n" +
+            "    tb_calendar        \n" +
+            "    -- 这里是限制返回最近一天的数据             \n" +
+            "    WHERE CONCAT(DATE_SUB(CURDATE(),INTERVAL 1 DAY),' 00:00:00')<= DATE(datelist)&&DATE(datelist)<=CONCAT(DATE_SUB(CURDATE(),INTERVAL 1 DAY),' 24:00:00')      \n" +
+            "    UNION ALL       \n" +
+            "    SELECT userLoginTime    \n" +
+            "    FROM        \n" +
+            "    tb_user       \n" +
+            "    WHERE userLoginTime >=CONCAT(DATE_SUB(CURDATE(),INTERVAL 1 DAY),' 00:00:00') AND userLoginTime<=CONCAT(DATE_SUB(CURDATE(),INTERVAL 1 DAY),' 24:00:00') AND userUseDevice <> NULL OR userUseDevice <> ''      \n" +
+            "    GROUP BY userLoginTime       \n" +
+            "    ) a       \n" +
+            "GROUP BY ddate")
     List<EquipmentStatisticsResponse> yestodayEquipmentStatistics();
 
     /*实现一周的新增设备的统计*/
