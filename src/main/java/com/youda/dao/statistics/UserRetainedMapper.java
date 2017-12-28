709634d7b36f4d19f36caf9e01c78370a69f0d3c
znlccy <756311868@qq.com>
2017-12-28 15:54:25 +0800

    实现用户留存统计

--- src/main/java/com/youda/dao/statistics/UserRetainedMapper.java
+++ src/main/java/com/youda/dao/statistics/UserRetainedMapper.java
@@ -19,17 +19,114 @@ import java.util.List;
 public interface UserRetainedMapper {
 
     /*实现自定义日期用户留存统计*/
-    @Select("SELECT (today.todayUserRetained/customrate.customUserRetained)*100 AS userRetainedCount,#{endTime} AS ddate FROM \n" +
-            "(SELECT COUNT(*) AS customUserRetained,CURDATE() AS ddate FROM tb_user\n" +
-            "WHERE userLoginTime>=CONCAT(#{beginTime},' 00:00:00') AND userLoginTime<=CONCAT(#{endTime},' 24:00:00')) AS customrate,\n" +
-            " (SELECT COUNT(*) AS todayUserRetained,CURDATE() AS ddate FROM tb_user WHERE userLoginTime >=CONCAT(CURDATE(),' 00:00:00') \n" +
-            "AND userLoginTime<=CONCAT(CURDATE(),' 24:00:00')) AS today")
+    @Select("SELECT userActive.ddate AS ddate,(userActive.userActiveCount/userNew.newUserCount)*100 AS userRetainedCount \n" +
+            "FROM\n" +
+            "(\t\n" +
+            "\tSELECT\n" +
+            "\t    DATE(dday) ddate,\n" +
+            "\t    COUNT(*) - 2 AS userActiveCount\n" +
+            "\tFROM\n" +
+            "\t(\n" +
+            "\t(\n" +
+            "\t   SELECT datelist AS dday\n" +
+            "\t   FROM tb_calendar \n" +
+            "\t   -- 这里是限制返回最近30天的数据\n" +
+            "\t   -- where  DATE_SUB(CURDATE(), INTERVAL 1 DAY) <= date(datelist)&&date(datelist)<=CURDATE() \n" +
+            "\t   WHERE  CONCAT(#{statisticsRequest.beginTime},' 00:00:00') <= DATE(datelist)&&DATE(datelist)<=CONCAT(#{statisticsRequest.endTime},' 23:59:59')\n" +
+            "\t)\n" +
+            "\tUNION ALL\n" +
+            "\t(\n" +
+            "\t   SELECT userLoginTime FROM tb_user ,(\n" +
+            "\t   SELECT phone FROM tb_channel_user WHERE channelId IN \n" +
+            "\t   (SELECT channelId FROM tb_gamechannel WHERE gameId IN (SELECT gameId FROM tb_game WHERE gameName=#{statisticsRequest.gameName}))) AS a \n" +
+            "\t   WHERE userLoginTime >=CONCAT(#{statisticsRequest.beginTime},' 00:00:00') && userLoginTime<=CONCAT(#{statisticsRequest.endTime},' 23:59:59') AND userName=a.phone AND userUseDevice=#{statisticsRequest.userUseDevice} \n" +
+            "\t   GROUP BY userLoginTime\n" +
+            "\t   )\n" +
+            "\t) AS a\n" +
+            "\tGROUP BY ddate\n" +
+            ") AS userActive\n" +
+            "RIGHT JOIN\n" +
+            "(\n" +
+            "\tSELECT\n" +
+            "\t    DATE(dday) ddate,\n" +
+            "\t    COUNT(*) - 2 AS newUserCount\n" +
+            "\tFROM\n" +
+            "\t(\n" +
+            "\t(\n" +
+            "\t   SELECT datelist AS dday\n" +
+            "\t   FROM tb_calendar \n" +
+            "\t   -- 这里是限制返回最近30天的数据\n" +
+            "\t   -- where  DATE_SUB(CURDATE(), INTERVAL 1 DAY) <= date(datelist)&&date(datelist)<=CURDATE() \n" +
+            "\t   WHERE  CONCAT(#{statisticsRequest.beginTime},' 00:00:00') <= DATE(datelist)&&DATE(datelist)<=CONCAT(#{statisticsRequest.endTime},' 23:59:59')\n" +
+            "\t)\n" +
+            "\tUNION ALL\n" +
+            "\t(\n" +
+            "\t   SELECT userRegisteredTime FROM tb_user ,(\n" +
+            "\t   SELECT phone FROM tb_channel_user WHERE channelId IN \n" +
+            "\t   (SELECT channelId FROM tb_gamechannel WHERE gameId IN (SELECT gameId FROM tb_game WHERE gameName=#{statisticsRequest.gameName}))) AS a \n" +
+            "\t   WHERE userRegisteredTime >=CONCAT(#{statisticsRequest.beginTime},' 00:00:00') && userRegisteredTime<=CONCAT(#{statisticsRequest.endTime},' 23:59:59') AND userName=a.phone AND userUseDevice=#{statisticsRequest.userUseDevice} \n" +
+            "\t   GROUP BY userRegisteredTime\n" +
+            "\t   )\n" +
+            "\t) AS a\n" +
+            "\tGROUP BY ddate\n" +
+            ") AS userNew\n" +
+            "ON userActive.ddate = userNew.ddate\n" +
+            "GROUP BY userActive.ddate")
     List<UserRetainedResponse> customTime(@Param("statisticsRequest") StatisticsRequest statisticsRequest);
 
     /*实现查询所有用户留存的统计*/
-    @Select("SELECT (today.todayUserRetained/allrate.allUserRetained)*100 AS userRetainedCount,CURDATE() AS ddate FROM \n" +
-            "(SELECT COUNT(*) AS allUserRetained, CURDATE() AS ddate FROM tb_user WHERE userLoginTime <> NULL) AS allrate,\n" +
-            " (SELECT COUNT(*) AS todayUserRetained,CURDATE() AS ddate FROM tb_user WHERE userLoginTime >=CONCAT(CURDATE(),' 00:00:00') \n" +
-            "AND userLoginTime<=CONCAT(CURDATE(),' 24:00:00')) AS today\n")
+    @Select("SELECT userActive.ddate AS ddate,(userActive.userActiveCount/userNew.newUserCount)*100 AS userRetainedCount \n" +
+            "FROM\n" +
+            "(\t\n" +
+            "\tSELECT\n" +
+            "\t    DATE(dday) ddate,\n" +
+            "\t    COUNT(*) - 2 AS userActiveCount\n" +
+            "\tFROM\n" +
+            "\t(\n" +
+            "\t(\n" +
+            "\t   SELECT datelist AS dday\n" +
+            "\t   FROM tb_calendar \n" +
+            "\t   -- 这里是限制返回最近30天的数据\n" +
+            "\t   -- where  DATE_SUB(CURDATE(), INTERVAL 1 DAY) <= date(datelist)&&date(datelist)<=CURDATE() \n" +
+            "\t   WHERE  CONCAT(#{statisticsRequest.beginTime},' 00:00:00') <= DATE(datelist)&&DATE(datelist)<=CONCAT(#{statisticsRequest.endTime},' 23:59:59')\n" +
+            "\t)\n" +
+            "\tUNION ALL\n" +
+            "\t(\n" +
+            "\t   SELECT userLoginTime FROM tb_user ,(\n" +
+            "\t   SELECT phone FROM tb_channel_user WHERE channelId IN \n" +
+            "\t   (SELECT channelId FROM tb_gamechannel WHERE gameId IN (SELECT gameId FROM tb_game WHERE gameName=#{statisticsRequest.gameName}))) AS a \n" +
+            "\t   WHERE userLoginTime >=CONCAT(#{statisticsRequest.beginTime},' 00:00:00') && userLoginTime<=CONCAT(#{statisticsRequest.endTime},' 23:59:59') AND userName=a.phone\n" +
+            "\t   GROUP BY userLoginTime\n" +
+            "\t   )\n" +
+            "\t) AS a\n" +
+            "\tGROUP BY ddate\n" +
+            ") AS userActive\n" +
+            "RIGHT JOIN\n" +
+            "(\n" +
+            "\tSELECT\n" +
+            "\t    DATE(dday) ddate,\n" +
+            "\t    COUNT(*) - 2 AS newUserCount\n" +
+            "\tFROM\n" +
+            "\t(\n" +
+            "\t(\n" +
+            "\t   SELECT datelist AS dday\n" +
+            "\t   FROM tb_calendar \n" +
+            "\t   -- 这里是限制返回最近30天的数据\n" +
+            "\t   -- where  DATE_SUB(CURDATE(), INTERVAL 1 DAY) <= date(datelist)&&date(datelist)<=CURDATE() \n" +
+            "\t   WHERE  CONCAT(#{statisticsRequest.beginTime},' 00:00:00') <= DATE(datelist)&&DATE(datelist)<=CONCAT(#{statisticsRequest.endTime},' 23:59:59')\n" +
+            "\t)\n" +
+            "\tUNION ALL\n" +
+            "\t(\n" +
+            "\t   SELECT userRegisteredTime FROM tb_user ,(\n" +
+            "\t   SELECT phone FROM tb_channel_user WHERE channelId IN \n" +
+            "\t   (SELECT channelId FROM tb_gamechannel WHERE gameId IN (SELECT gameId FROM tb_game WHERE gameName=#{statisticsRequest.gameName}))) AS a \n" +
+            "\t   WHERE userRegisteredTime >=CONCAT(#{statisticsRequest.beginTime},' 00:00:00') && userRegisteredTime<=CONCAT(#{statisticsRequest.endTime},' 23:59:59') AND userName=a.phone \n" +
+            "\t   GROUP BY userRegisteredTime\n" +
+            "\t   )\n" +
+            "\t) AS a\n" +
+            "\tGROUP BY ddate\n" +
+            ") AS userNew\n" +
+            "ON userActive.ddate = userNew.ddate\n" +
+            "GROUP BY userActive.ddate")
     List<UserRetainedResponse> all(@Param("statisticsRequest") StatisticsRequest statisticsRequest);
 }
