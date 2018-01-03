d13b9b8065c0ae64c6f425c0e4392cdfe97ec0ff
znlccy <756311868@qq.com>
2018-01-03 14:57:38 +0800

    重新实现付费玩家统计

--- src/main/java/com/youda/dao/statistics/PayMapper.java
+++ src/main/java/com/youda/dao/statistics/PayMapper.java
@@ -276,18 +276,20 @@ public interface PayMapper {
     /*实现任意日期支付玩家统计*/
     @Select("SELECT    \n" +
             "     DISTINCT DATE_FORMAT(StatisticsDate,'%Y-%m-%d') AS StatisticsDate,     \n" +
-            "     IFNULL(COUNT(*)-1,0) AS payCount   \n" +
+            "     IFNULL(payCount,0) AS payCount\n" +
             "FROM      \n" +
             "(    \n" +
             "    SELECT DISTINCT DATE(payRecordTime) AS StatisticsDate,     \n" +
-            "    SUM(payRecordTotalAmount) AS incomeTotalMoney     \n" +
+            "    SUM(payRecordTotalAmount) AS incomeTotalMoney,     \n" +
+            "    COUNT(DISTINCT payRecordUser) AS payCount     \n" +
             "    FROM tb_payrecord      \n" +
             "    WHERE payRecordTime>=DATE_FORMAT(#{statisticsRequest.beginTime},'%Y-%m-%d') && payRecordTime<=DATE_FORMAT(#{statisticsRequest.endTime},'%Y-%m-%d') AND gameChannelId=#{statisticsRequest.gameChannelId} AND userUseDevice=#{statisticsRequest.userUseDevice} AND payRecordStatus='1' \n" +
-            "    GROUP BY payRecordTime  \n" +
+            "    GROUP BY DATE(payRecordTime)  \n" +
             "UNION    \n" +
             "   (     \n" +
             "    SELECT DISTINCT datelist AS StatisticsDate,     \n" +
-            "    payRecordTotalAmount AS incomeTotalMoney     \n" +
+            "    payRecordTotalAmount AS incomeTotalMoney,     \n" +
+            "    incomeCount AS payCount     \n" +
             "    FROM tb_income      \n" +
             "    WHERE DATE_FORMAT(#{statisticsRequest.beginTime},'%Y-%m-%d')<= DATE(datelist)&&DATE(datelist)<=DATE_FORMAT(#{statisticsRequest.endTime},'%Y-%m-%d') \n" +
             "   )      \n" +
@@ -298,18 +300,20 @@ public interface PayMapper {
     /*实现全部的付费玩家统计*/
     @Select("SELECT    \n" +
             "     DISTINCT DATE_FORMAT(StatisticsDate,'%Y-%m-%d') AS StatisticsDate,     \n" +
-            "     IFNULL(COUNT(*)-1,0) AS payCount   \n" +
+            "     IFNULL(payCount,0) AS payCount\n" +
             "FROM      \n" +
             "(    \n" +
             "    SELECT DISTINCT DATE(payRecordTime) AS StatisticsDate,     \n" +
-            "    SUM(payRecordTotalAmount) AS incomeTotalMoney     \n" +
+            "    SUM(payRecordTotalAmount) AS incomeTotalMoney,     \n" +
+            "    COUNT(DISTINCT payRecordUser) AS payCount     \n" +
             "    FROM tb_payrecord      \n" +
             "    WHERE payRecordTime>=DATE_FORMAT(#{statisticsRequest.beginTime},'%Y-%m-%d') && payRecordTime<=DATE_FORMAT(#{statisticsRequest.endTime},'%Y-%m-%d') AND gameChannelId=#{statisticsRequest.gameChannelId} AND userUseDevice IS NOT NULL AND payRecordStatus='1' \n" +
-            "    GROUP BY payRecordTime  \n" +
+            "    GROUP BY DATE(payRecordTime)  \n" +
             "UNION    \n" +
             "   (     \n" +
             "    SELECT DISTINCT datelist AS StatisticsDate,     \n" +
-            "    payRecordTotalAmount AS incomeTotalMoney     \n" +
+            "    payRecordTotalAmount AS incomeTotalMoney,     \n" +
+            "    incomeCount AS payCount     \n" +
             "    FROM tb_income      \n" +
             "    WHERE DATE_FORMAT(#{statisticsRequest.beginTime},'%Y-%m-%d')<= DATE(datelist)&&DATE(datelist)<=DATE_FORMAT(#{statisticsRequest.endTime},'%Y-%m-%d') \n" +
             "   )      \n" +
