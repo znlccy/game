e8b83f69cb6c2384fde61f16c84b189bd5b46084
znlccy <756311868@qq.com>
2018-01-17 03:07:45 +0800

    修复用户新增接口统计的功能

--- src/main/java/com/youda/dao/statistics/UserNewMapper.java
+++ src/main/java/com/youda/dao/statistics/UserNewMapper.java
@@ -12,43 +12,47 @@ import java.util.List;
 public interface UserNewMapper {
 
     /*实现自定义日期查询*/
-    @Select("SELECT    \n" +
-            "DISTINCT DATE_FORMAT(StatisticsDate,'%Y-%m-%d') AS StatisticsDate,IFNULL(userNewCount,0) AS userNewCount    \n" +
-            "FROM     \n" +
-            "(    \n" +
-            "   SELECT DISTINCT DATE(userRegistedTime) AS StatisticsDate,    \n" +
-            "   COUNT(DISTINCT userId) AS userNewCount     \n" +
-            "   FROM tb_user_caculator     \n" +
-            "   WHERE userRegistedTime>=DATE_FORMAT(CONCAT(#{statisticsRequest.beginTime},' 00:00:00'),'%Y-%m-%d %H:%i:%s') && userRegistedTime<=DATE_FORMAT(CONCAT(#{statisticsRequest.endTime},' 23:59:59'),'%Y-%m-%d %H:%i:%s') AND gameChannelId=#{statisticsRequest.gameChannelId} and userUseDevice=#{statisticsRequest.userUseDevice}\n" +
-            "   GROUP BY userRegistedTime\n" +
-            "UNION    \n" +
-            "(    \n" +
-            "   SELECT DISTINCT datelist AS StatisticsDate,    \n" +
-            "   payRecordTotalAmount AS userNewCount    \n" +
-            "   FROM tb_income     \n" +
-            "   WHERE DATE_FORMAT(CONCAT(#{statisticsRequest.beginTime},' 00:00:00'),'%Y-%m-%d %H:%i:%s') <= DATE(datelist)&&DATE(datelist)<=DATE_FORMAT(CONCAT(#{statisticsRequest.endTime},' 23:59:59'),'%Y-%m-%d %H:%i:%s') \n" +
-            ")     \n" +
-            ") AS b    \n" +
+    @Select("SELECT     \n" +
+            "DISTINCT DISTINCT StatisticsDate AS StatisticsDate,IFNULL(SUM(userNewCount),0) AS userNewCount     \n" +
+            "FROM      \n" +
+            "(  \n" +
+            "   SELECT a.StatisticsDate AS StatisticsDate, SUM(a.userNewCount) AS userNewCount FROM \n" +
+            "   (SELECT DATE(userRegistedTime) AS StatisticsDate,     \n" +
+            "   COUNT(DISTINCT userId) AS userNewCount      \n" +
+            "   FROM tb_user_caculator      \n" +
+            "   WHERE userRegistedTime>=DATE_FORMAT(CONCAT(#{statisticsRequest.beginTime},' 00:00:00'),'%Y-%m-%d %H:%i:%s') && userRegistedTime<=DATE_FORMAT(CONCAT(#{statisticsRequest.endTime},' 23:59:59'),'%Y-%m-%d %H:%i:%s') AND gameChannelId=#{statisticsRequest.gameChannelId} AND userUseDevice=#{statisticsRequest.userUseDevice}\n" +
+            "   GROUP BY userRegistedTime) AS a\n" +
+            "   GROUP BY a.StatisticsDate\n" +
+            "UNION     \n" +
+            "(     \n" +
+            "   SELECT DISTINCT datelist AS StatisticsDate,     \n" +
+            "   payRecordTotalAmount AS userNewCount     \n" +
+            "   FROM tb_income      \n" +
+            "   WHERE DATE_FORMAT(CONCAT(#{statisticsRequest.beginTime},' 00:00:00'),'%Y-%m-%d %H:%i:%s') <= DATE(datelist)&&DATE(datelist)<=DATE_FORMAT(CONCAT(#{statisticsRequest.endTime},' 23:59:59'),'%Y-%m-%d %H:%i:%s')  \n" +
+            ")      \n" +
+            ") AS b     \n" +
             "GROUP BY StatisticsDate")
     List<UserNewResponse> cudtomTime(@Param("statisticsRequest") StatisticsRequest statisticsRequest);
 
-    @Select("SELECT    \n" +
-            "DISTINCT DATE_FORMAT(StatisticsDate,'%Y-%m-%d') AS StatisticsDate,IFNULL(userNewCount,0) AS userNewCount    \n" +
-            "FROM     \n" +
-            "(    \n" +
-            "   SELECT DISTINCT DATE(userRegistedTime) AS StatisticsDate,    \n" +
-            "   COUNT(DISTINCT userId) AS userNewCount     \n" +
-            "   FROM tb_user_caculator     \n" +
-            "   WHERE userRegistedTime>=DATE_FORMAT(CONCAT(#{statisticsRequest.beginTime},' 00:00:00'),'%Y-%m-%d %H:%i:%s') && userRegistedTime<=DATE_FORMAT(CONCAT(#{statisticsRequest.endTime},' 23:59:59'),'%Y-%m-%d %H:%i:%s') AND gameChannelId=#{statisticsRequest.gameChannelId} and userUseDevice is not null    \n" +
-            "   GROUP BY userRegistedTime\n" +
-            "UNION    \n" +
-            "(    \n" +
-            "   SELECT DISTINCT datelist AS StatisticsDate,    \n" +
-            "   payRecordTotalAmount AS userNewCount    \n" +
-            "   FROM tb_income     \n" +
-            "   WHERE DATE_FORMAT(CONCAT(#{statisticsRequest.beginTime},' 00:00:00'),'%Y-%m-%d %H:%i:%s')<= DATE(datelist)&&DATE(datelist)<=DATE_FORMAT(CONCAT(#{statisticsRequest.endTime},' 23:59:59'),'%Y-%m-%d %H:%i:%s')  \n" +
-            ")     \n" +
-            ") AS b    \n" +
+    @Select("SELECT     \n" +
+            "DISTINCT DISTINCT StatisticsDate AS StatisticsDate,IFNULL(SUM(userNewCount),0) AS userNewCount     \n" +
+            "FROM      \n" +
+            "(  \n" +
+            "   SELECT a.StatisticsDate AS StatisticsDate, SUM(a.userNewCount) AS userNewCount FROM \n" +
+            "   (SELECT DATE(userRegistedTime) AS StatisticsDate,     \n" +
+            "   COUNT(DISTINCT userId) AS userNewCount      \n" +
+            "   FROM tb_user_caculator      \n" +
+            "   WHERE userRegistedTime>=DATE_FORMAT(CONCAT(#{statisticsRequest.beginTime},' 00:00:00'),'%Y-%m-%d %H:%i:%s') && userRegistedTime<=DATE_FORMAT(CONCAT(#{statisticsRequest.endTime},' 23:59:59'),'%Y-%m-%d %H:%i:%s') AND gameChannelId=#{statisticsRequest.gameChannelId} AND userUseDevice IS NOT NULL\n" +
+            "   GROUP BY userRegistedTime) AS a\n" +
+            "   GROUP BY a.StatisticsDate\n" +
+            "UNION     \n" +
+            "(     \n" +
+            "   SELECT DISTINCT datelist AS StatisticsDate,     \n" +
+            "   payRecordTotalAmount AS userNewCount     \n" +
+            "   FROM tb_income      \n" +
+            "   WHERE DATE_FORMAT(CONCAT(#{statisticsRequest.beginTime},' 00:00:00'),'%Y-%m-%d %H:%i:%s') <= DATE(datelist)&&DATE(datelist)<=DATE_FORMAT(CONCAT(#{statisticsRequest.endTime},' 23:59:59'),'%Y-%m-%d %H:%i:%s')  \n" +
+            ")      \n" +
+            ") AS b     \n" +
             "GROUP BY StatisticsDate")
     List<UserNewResponse> all(@Param("statisticsRequest") StatisticsRequest statisticsRequest);
 }
