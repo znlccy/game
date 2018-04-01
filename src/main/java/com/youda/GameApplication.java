27dfc1360d2e613b0f2e02fe9a8f6b390309a1dc
znlccy <756311868@qq.com>
2018-04-01 15:47:36 +0800

    添加Redis缓存机制

--- src/main/java/com/youda/GameApplication.java
+++ src/main/java/com/youda/GameApplication.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.cors.CorsConfiguration;
@@ -26,6 +27,7 @@ import java.util.TimerTask;
 @SpringBootApplication
 @MapperScan("com.youda.dao")
 @EnableScheduling
+@EnableCaching
 public class GameApplication {
 
     /**
